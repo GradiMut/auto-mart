486375feaad65727bf50e529a79e6fa6558f1aae
Gradia Mutambay <gradimutdev@gmail.com>
2019-06-12 10:32:31 +0300

    bug(fixing nav bar): Add elevation (box-shadow)
    this commit contain the chnage that separate the nav bar to the body
    
    [finishes #166629860]

--- UI/js/app.js
+++ UI/js/app.js
@@ -96,3 +96,23 @@ var myTabs = tabs({
 });
 
 myTabs.init();
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("reportForm");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
